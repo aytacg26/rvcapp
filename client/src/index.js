13a9fa0bd24b342e9cf21628a8ebd3ecf2e0aa7f
aytacguley <aytacguley@dklassix.com>
2021-09-26 14:25:27 +0300

    App wrapped with ContextProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ContextProvider } from './SocketContext';
 import App from './App';
 import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
