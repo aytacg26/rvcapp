f2202c1923115ebeff0253bfef8a43515a779671
aytacguley <aytacguley@dklassix.com>
2021-09-26 13:39:23 +0300

    init styles added to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+import VideoPlayer from './components/VideoPlayer';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
 
 const App = () => {
-  return <div>React Video Chat App</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <AppBar position='static' color='inherit' className={classes.appBar}>
+        <Typography variant='h4' align='center'>
+          RVC - Connect to World
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+      {/**VIDEO PLAYER */}
+      {/**Options -> Notifications */}
+    </div>
+  );
 };
 
 export default App;
